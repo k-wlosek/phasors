c8077de35e426e4c8f12289de1ae14df8cfcbf5f
kw <115936263+k-wlosek@users.noreply.github.com>
2023-11-19 17:21:33 +0100

    feat(symbol): Add class for handling one symbol type

---
+++ src/symbol.py
@@ -0,0 +1,68 @@
+import numpy as np
+import dataclasses
+
+
+# noinspection PyMethodMayBeStatic
+@dataclasses.dataclass
+class Symbol:
+    """
+    Represents a symbol in a phasor diagram.
+    ex. [(1, 0, 'V_1', 'red'), (1, 1.25, 'V_2', 'blue'), (1, 3.14, 'V_3', 'green')]
+    All angles are in radians.
+
+    One symbol is represented by a list of tuples, where each tuple represents a phasor.
+    """
+    def __init__(self, data_list):
+        phasors = []
+        for phasor in data_list:
+            length, angle, annotation, color = phasor
+            angle_rad = np.deg2rad(angle)
+            phasors.append((length, angle_rad, self._prepare_annotation(annotation), color))
+        self.phasors = phasors
+
+    def _prepare_annotation(self, annotation: str) -> str:
+        """
+        Prepares annotation for LaTeX rendering.
+        :param annotation: string for preparation
+        :return: prepared string
+        :rtype: str
+        """
+        underscore_location = annotation.find('_')
+        return "$" + annotation[:underscore_location + 1] + "{" + annotation[underscore_location + 1:] + "}$"
+
+    @staticmethod
+    def from_list(data_list):
+        """
+        Returns a list of symbols from a list of lists of tuples representing data for each symbol.
+        :type data_list: list[list[tuple[float, float, str, str]]]
+        :param data_list: list of lists of tuples representing data for each symbol
+        :return: list of symbols
+        """
+        symbols = []
+        for symbol_list in data_list:
+            symbols.append(Symbol(symbol_list))
+        return symbols
+
+    def __iter__(self):
+        return iter(self.phasors)
+
+    def __getitem__(self, item):
+        return self.phasors[item]
+
+    def __len__(self):
+        return len(self.phasors)
+
+    def __repr__(self):
+        return f'Symbol({repr(self.phasors)})'
+
+    def __str__(self):
+        str_rep = ""
+        for value, angle, annotation, color in self.phasors:
+            str_rep += f'Value: {value}, angle (rad): {angle}, annotation: {annotation}, color: {color}\n'
+        return str_rep
+
+    def __eq__(self, other):
+        return self.phasors == other.phasors
+
+    def __ne__(self, other):
+        return self.phasors != other.phasors
