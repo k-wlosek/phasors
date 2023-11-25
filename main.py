71288a0937677dcf3afe9275c9a35f5889ca5042
kw <115936263+k-wlosek@users.noreply.github.com>
2023-11-25 01:39:42 +0100

    feat(project): Update main.py to show sample usage of the project

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 """
     Main file of the project.
 """
+from typing import Union
+import base64
 from src.diagram import Diagram
 from src.symbol import Symbol
 
-phasors_list: list[list[tuple[float, float, str, str]]] = [
-    [
-        (66.27, 0, "U_R1", "green"),
-        (169.13, 90, "U_L1", "blue"),
-        (-87.92, 90, "U_M", "black"),
-        (58.76, 0, "U_R2", "green"),
-        (162, 90, "U_L2", "blue"),
-        (-87.92, 90, "U_M", "black"),
-        (200, 57.25, "U", "pink")
-    ],
-    [
-        (0.53, 0, "I", "red")
-    ]
-]
-
-phasors_list2 = [
+phasors_list: list[list[Union[tuple[float, float, str, str]], str]] = [
     [
         (66.48, 0, "U_R1", "green"),
         (169.67, 90, "U_L1", "blue"),
@@ -27,31 +14,21 @@ phasors_list2 = [
         (58.94, 0, "U_R2", "green"),
         (162.51, 90, "U_L2", "blue"),
         (-88.2, 90, "U_M", "black"),
-        (200, 51.16, "U", "pink")
+        (200, 51.16, "U", "pink"),
+        "V"
     ],
     [
-        (0.53, 0, "I", "red")
+        (0.53, 0, "I", "red"),
+        "A"
     ]
 ]
-# phasors_list2 = [
-#     [
-#         (25.38, 0, "U_R1", "green"),
-#         (64.78, 90, "U_L1", "blue"),
-#         (33.68, 90, "U_M", "black"),
-#         (22.51, 0, "U_R2", "green"),
-#         (62.05, 90, "U_L2", "blue"),
-#         (33.68, 90, "U_M", "black"),
-#         (200, 76.15, "U", "pink")
-#     ],
-#     [
-#         (0.2, 0, "I", "red")
-#     ]
-# ]
 
 symbols: list[Symbol] = Symbol.from_list(phasors_list)
-d = Diagram(symbols, "Wykres fazorowy dla pomiaru 1 w sprzężeniu przeciwnym")
+d = Diagram(symbols, "Wykres fazorowy dla pomiaru 1 w sprzężeniu przeciwnym", [(0, 1, "red")])
 d.create()
 d.show()
-# d.save("zad2_1_wrong.png")
+d.save("zad2_1.png", "png")
+d.save("zad2_1.svg", "svg")
 byte = d.save_as_bytes("svg")
-print(byte)
+byte.seek(0)
+print(base64.b64encode(byte.read()).decode('UTF-8'))
